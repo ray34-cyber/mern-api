9a4f223cbfda1e7b6630a0a10bd91151ba8aa672
ray34-cyber <rancha.adzany1@gmail.com>
2023-02-21 09:54:39 +0800

    menambahkan cors dengan cara package

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const authRoutes = require("./src/routes/auth");
@@ -31,12 +32,13 @@ app.use(bodyParser.json());
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET , POST , PUT, PATCH, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET , POST , PUT, PATCH, DELETE, OPTIONS");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+app.use(cors());
 
 app.use("/v1/auth", authRoutes);
 app.use("/v1/blog", blogRoutes);
